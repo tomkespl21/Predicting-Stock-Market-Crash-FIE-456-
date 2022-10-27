4905c43b573ad02c3e58fb0db1740e1580e86ede
Tomke Splettstößer <113780717+tomkespl21@users.noreply.github.com>
2022-10-28 00:40:05 +0200

    Add files via upload

--- project code.R
+++ project code.R
@@ -366,35 +366,7 @@ df <- data[,4:31]
 
 cor_ma <- cor(df)
 
-corr_simple <- function(data=df,sig=0.5){
-   #convert data to numeric in order to run correlations
-   #convert to factor first to keep the integrity of the data - each value will become a number rather than turn into NA
-   df_cor <- data %>% mutate_if(is.character, as.factor)
-   df_cor <- df_cor %>% mutate_if(is.factor, as.numeric)
-   #run a correlation and drop the insignificant ones
-   corr <- cor(df_cor)
-   #prepare to drop duplicates and correlations of 1     
-   corr[lower.tri(corr,diag=TRUE)] <- NA 
-   #drop perfect correlations
-   corr[corr == 1] <- NA 
-   #turn into a 3-column table
-   corr <- as.data.frame(as.table(corr))
-   #remove the NA values from above 
-   corr <- na.omit(corr) 
-   #select significant values  
-   corr <- subset(corr, abs(Freq) > sig) 
-   #sort by highest correlation
-   corr <- corr[order(-abs(corr$Freq)),] 
-   #print table
-   print(corr)
-   #turn corr back into matrix in order to plot with corrplot
-   mtx_corr <- reshape2::acast(corr, Var1~Var2, value.var="Freq")
-   
-   #plot correlations visually
-   corrplot(mtx_corr, is.corr=FALSE, tl.col="black", na.label=" ")
-}
-
-corr_simple(df,0.5)
+corrplot(cor_ma,tl.cex=0.6,tl.offset = 1)
 
 # based on this plot we excluded variables that were extremely highly correlated
 # for obvious reasons and probably dont give useful extra information 
@@ -414,7 +386,7 @@ corr_simple(df,0.5)
 # 4. maybe smallers run-time 
 
 
-# change upordown to numeric
+# change factor variable to numeric
 df2 <- train1
 
 df2$y1=as.numeric(df2$y1)
